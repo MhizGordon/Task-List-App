d91770a6b87f48367663f5f262f48af033302db6
Gloria Gordon-Mensah <gordonmensahgloria1@gmail.com>
2025-07-10 19:38:52 +0100

    Home-page

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,9 +1,25 @@
-const Home = () => (
-  <div className="container mt-4">
-    <h1>Welcome to the Home Page!</h1>
-  </div>
-);
+import { FcHome, FcTodoList, FcCheckmark } from "react-icons/fc";
+import { Container, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./home.css";
+
+
+const Home = () => {
+  return (
+    <Container className="home-container"> 
+      <h1><FcHome className="Welcome" />    Welcome to Your Task App</h1>
+      <p>Organize your tasks with a colorful, simple interface.</p>
+
+      <ul className="list">
+        <li className="icon"><FcTodoList className="icons" /> Real-time task updates</li>
+        <li className="icon"><FcCheckmark className="icons" /> Check off completed tasks</li>
+      </ul>
+
+      <Link to="/tasks">
+        <Button variant="contained" size="lg" className="button">Go to Tasks</Button>
+      </Link>
+    </Container>
+  );
+};
 
 export default Home;
-import React from 'react';
-  // This import is necessary for React to work properly
